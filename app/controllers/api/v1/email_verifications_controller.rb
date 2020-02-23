23718cb021e082725f076cd6de6f78d106d4643d
Samantha Karam <skaram@wayfair.com>
2020-02-23 14:48:52 -0500

    Finished all tasks - fixed failing test, altered API to accept 1 to 10 email addresses, locked down api to only active users, and added basic UI

--- app/controllers/api/v1/email_verifications_controller.rb
+++ app/controllers/api/v1/email_verifications_controller.rb
@@ -1,16 +1,28 @@
 class Api::V1::EmailVerificationsController < ApplicationController
   skip_before_action :verify_authenticity_token
+  before_action :require_active_user
   before_action :set_verification, only: [:show, :destroy]
-
 
   # POST /email_verifications
   def create
-   @email_verification = EmailVerification.new(email_verification_params)
-   if @email_verification.save
-    render json: @email_verification, status: :created, location: api_v1_email_verifications_url(@email_verification)
-   else
-    render json: @email_verification.errors, status: :unprocessable_entity
-   end
+    email_verifications = []
+    email_addresses = email_verification_params['email_verification']
+    if(email_addresses.length() > 10 || email_addresses.length() < 1) 
+        email_verification_error = {error: "Incorrect number of emails input."}
+        render json: email_verification_error, status: :bad_request
+        return
+    end
+
+   for email_address in email_addresses do
+     email_verification = EmailVerification.new(email_address)
+     if email_verification.save
+      email_verifications.push(email_verification)
+     else
+      render json: email_verification.errors, status: :unprocessable_entity
+      return
+     end
+    end
+    render json: email_verifications, status: :created
   end
 
   # GET /email_verifications
@@ -40,10 +52,23 @@ class Api::V1::EmailVerificationsController < ApplicationController
   private
 
   def email_verification_params
-    params.require(:email_verification).permit(:address,:id)
+    params.permit(:token, :email_verification => [:address])
   end
 
   def set_verification
    @email_verification = EmailVerification.where(id: params[:id]).first
+  end
+
+  def require_active_user
+    @user = User.where(uuid: params[:token]).first
+    user_verification_error = {errors: "Sorry, you are not authorized to use this data."}
+
+    if @user.present?
+      if @user[:status] != "active"
+        render json: user_verification_error, status: :unauthorized
+      end
+    else
+      render json: user_verification_error, status: :unauthorized
+    end
   end
 end
--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,8 +1,9 @@
 class MainController < ApplicationController
   def home
     if !params[:address].blank?
-      valid = EmailAddress.new(address: params[:address].to_s).check_status
-      flash.now[:success] = valid ? "Email is valid!" : "Email is not valid!"
+      valid = EmailAddress.create(address: params[:address].to_s).check_status
+      flash.now[:success] = valid == "valid" ? "Email is valid!" : "Email is not valid!"
+      @address = params[:address].to_s
     end
   end
 end
